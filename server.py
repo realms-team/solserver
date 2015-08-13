6f2fd85b1a640f7841b68185b6fc3bd41f8d7467
Thomas Watteyne <twatteyne@gmail.com>
2015-08-12 18:09:21 -0700

    dummy bottle application

--- server.py
+++ server.py
@@ -4,6 +4,10 @@
 
 import sys
 import os
+import json
+
+import threading
+
 if __name__ == "__main__":
     here = sys.path[0]
     sys.path.insert(0, os.path.join(here, '..', 'Sol'))
@@ -11,6 +15,8 @@ if __name__ == "__main__":
 #============================ imports =========================================
 
 from   optparse                             import OptionParser
+
+import bottle
 
 import SolVersion
 import server_version
@@ -25,12 +31,45 @@ DEFAULT_PORT = 8080
 class Server(object):
     
     def __init__(self,port):
-        pass
+        
+        # local variables
+        self.web = bottle.Bottle()
+        
+        # initialize routes
+        self.web.route(path='/api/v1/echo.json',   method='GET', callback=self._webcb_echo_GET)
+        self.web.route(path='/api/v1/status.json', method='GET', callback=self._webcb_status_GET)
+        self.web.route(path='/api/v1/o.json',      method='PUT', callback=self._webcb_o_PUT)
+        
+        # start web server in separate thread
+        self.webThread = threading.Thread(
+            target = self.web.run,
+            kwargs = {
+                'host'          : 'localhost',
+                'port'          : port,
+                'quiet'         : True,
+                'debug'         : False,
+            }
+        )
+        self.webThread.start()
     
     #======================== public ==========================================
     
     #======================== private =========================================
-
+    
+    def _webcb_echo_GET(self):
+        bottle.response.status = 501
+        bottle.response.content_type = 'application/json'
+        return json.dumps({'error': 'Not Implemented yet :-('})
+    
+    def _webcb_status_GET(self):
+        bottle.response.status = 501
+        bottle.response.content_type = 'application/json'
+        return json.dumps({'error': 'Not Implemented yet :-('})
+    
+    def _webcb_o_PUT(self):
+        bottle.response.status = 501
+        bottle.response.content_type = 'application/json'
+        return json.dumps({'error': 'Not Implemented yet :-('})
 
 #============================ main ============================================
 
